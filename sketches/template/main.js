9bf043397901729163faaac5ac3ccc80f2673937
Bryan Houlton <brhoulton@gmail.com>
2020-06-22 18:57:46 -0400

    made a template

--- sketches/template/main.js
+++ sketches/template/main.js
@@ -1,1 +1,6 @@
-console.log('main.js connected')
+
+// Program Inits
+var canvas = document.querySelector('canvas')
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+var c = canvas.getContext("2d");
